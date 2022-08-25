7fc779e33537b57db86068f0de831a31483767f4
mdwisu <dwisusanto784@gmail.com>
2022-08-25 15:34:16 +0700

    membuat login

---
+++ backend/models/UserModel.js
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+import db from '../config/Database.js';
+
+const { DataTypes } = Sequelize;
+
+// membuat schema
+const Users = db.define('users',{
+    name:{
+        type: DataTypes.STRING
+    },
+    email:{
+        type: DataTypes.STRING
+    },
+    password:{
+        type: DataTypes.STRING
+    },
+    refresh_token:{
+        type: DataTypes.TEXT
+    },
+},{
+    freezeTableName: true
+})
+
+
+export default Users
